696ae27376c4b3782f4d49f01d9c8ad651d99382
Vitaly Timoshenko <timoshenkovyu@licard.com>
2022-05-25 15:16:42 +0300

    Initial commit

---
+++ Package.swift
@@ -0,0 +1,27 @@
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobile",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "/Users/vitaly/work/YandexMapsMobileSPM/YandexMapsMobile.xcframework.zip",
+            checksum: "78df3f9468d1b65d3c1e90b38db363134774211485b359bafdb5f34b0856c37f"
+        ),
+    ]
+)
